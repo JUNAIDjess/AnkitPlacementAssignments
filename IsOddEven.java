07a8d1cb6a798bca8db698bdc482481f86cf8ecd
JUNAIDjess <jessieairance1234@gmail.com>
2025-01-20 16:05:12 +0530

    firstQuestionSolved

---
+++ IsOddEven.java
@@ -0,0 +1,11 @@
+// Determining Even/Odd Numbers
+
+import java.util.Scanner;
+public class IsOddEven {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println("The Number " + num + " is " + ((num & 1) == 0 ? "Even" : "Odd"));
+    }
+}
